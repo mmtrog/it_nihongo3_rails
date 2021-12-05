3260cebf156f52bf82b7d9bf8946ae5f009f36ea
mmtrog <minhtrong2k@gmail.com>
2021-12-05 14:05:44 +0000

    Add avatar on users

--- db/migrate/20211205133428_add_name_and_age.rb
+++ db/migrate/20211205133428_add_name_and_age.rb
@@ -2,7 +2,7 @@ class AddNameAndAge < ActiveRecord::Migration[6.0]
   def change
       add_column :users, :name, :string
         add_column :users, :age, :integer
-        add_index :users, :name, unique: true
+        add_index :users, :name, unique: false
 
   end
 end
